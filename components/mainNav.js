6355d61e557f5bf5344730ae0d807386ec93ee6a
SaschaVII <sascha.kuhness@gmail.com>
2023-03-03 00:02:26 +0000

    Add navItems prop to mainNav component

--- components/mainNav.js
+++ components/mainNav.js
@@ -1,14 +1,15 @@
-const MainNav = ({ title }) => {
-    const navItemStyles = "text-primary-400 hover:text-primary-900 before:content-['<'] after:content-['_/>'] whitespace-nowrap";
-
+const MainNav = ({ title, navItems }) => {
     return (
         <nav className="flex flex-col sm:flex-row justify-between flex-wrap gap-2">
             <h1 className="text-5xl font-bold">{title}</h1>
             <div className="flex flex-col gap-2 sm:gap-10 sm:flex-row flex-wrap sm:self-center">
-                <a className={navItemStyles} href="#">About Me</a>
-                <a className={navItemStyles} href="#">Experience</a>
-                <a className={navItemStyles} href="#">Work</a>
-                <a className={navItemStyles} href="#">Contact</a>
+                {navItems.map(item => {
+                    return (
+                        <a key={item.id} className="text-primary-400 hover:text-primary-900 before:content-['<'] after:content-['_/>'] whitespace-nowrap" href={item.url}>
+                            {item.title}
+                        </a>
+                    )
+                })}
             </div>
         </nav>
     );
--- components/stylesShowcase.js
+++ components/stylesShowcase.js
@@ -2,58 +2,83 @@ import Button from "./button";
 import MainNav from "./mainNav";
 import ProjectCard from "./projectCard";
 
-const StylesShowcase = () => (
-    <div>
-        <h1 className="mb-2">Components</h1>
-        <section className="mb-2">
-            <div className="pl-4">
-                <h2>Buttons</h2>
+const StylesShowcase = () => {
+    const navItems = [
+        {
+            id: 0,
+            url: "#",
+            title: "About Me"
+        },
+        {
+            id: 1,
+            url: "#",
+            title: "Experience"
+        },
+        {
+            id: 2,
+            url: "#",
+            title: "Work"
+        },
+        {
+            id: 3,
+            url: "#",
+            title: "Contact"
+        }
+    ];
+
+    return (
+        <div>
+            <h1 className="mb-2">Components</h1>
+            <section className="mb-2">
                 <div className="pl-4">
-                    <Button>Primary</Button>
-                    <Button type="secondary">Secondary</Button>
+                    <h2>Buttons</h2>
+                    <div className="pl-4">
+                        <Button>Primary</Button>
+                        <Button type="secondary">Secondary</Button>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
 
-        <section className="mb-4">
-            <h1 className="mb-2">Main Nav</h1>
-            <div className="pl-4">
-                <MainNav title="Sascha Kuhness" />
-            </div>
-        </section>
-
-        <section className="mb-4">
-            <h1 className="mb-2">Project Card</h1>
-            <div className="pl-4 flex gap-10 flex-wrap">
-            <ProjectCard title="Project Title" thumbnailSrc="./placeholder375x375.png">
-                    <p>
-                        Irure occaecat mollit sunt consectetur aliqua minim culpa officia voluptate ipsum in adipisicing consequat est. Ex do enim laboris excepteur sunt officia cupidatat cillum. Proident et eu aliqua consectetur duis qui reprehenderit incididunt.
-                    </p>
-                    <Button className="mt-2">Open GitHub</Button>
-                </ProjectCard>
-                <ProjectCard title="Project Title" thumbnailSrc="./placeholder375x375.png">
-                    <p>
-                        Proident eiusmod dolore veniam sint duis sit et quis ut enim duis elit amet consequat. Labore anim ea sit ut. Ex est irure culpa fugiat id. Fugiat eiusmod commodo occaecat duis irure eu dolore et ut tempor. Esse qui commodo minim eiusmod ea magna labore Lorem commodo ullamco. Elit pariatur commodo occaecat commodo magna aliquip. Laborum magna eiusmod et sunt amet laboris in.
-                    </p>
-                    <Button className="mt-2">Open GitHub</Button>
-                </ProjectCard>
-            </div>
-        </section>
-
-        <section className="mb-4">
-            <h1 className="mb-2">Typography</h1>
-            <div className="pl-4">
+            <section className="mb-4">
+                <h1 className="mb-2">Main Nav</h1>
                 <div className="pl-4">
-                    <h1 className="text-5xl font-bold">Title</h1>
-                    <h1>Header 1</h1>
-                    <h2>Header 2</h2>
-                    <p>
-                        This is what a paragraph looks like. Dolor magna exercitation ad ipsum magna anim ut Lorem adipisicing mollit enim anim. Ut ut esse est consectetur pariatur exercitation do occaecat. Cillum eu id mollit est magna proident ullamco amet. Ea reprehenderit laboris dolor non aute officia dolor. Sunt proident officia et dolore mollit est.
-                    </p>
+                    <MainNav title="Sascha Kuhness" navItems={navItems} />
                 </div>
-            </div>
-        </section>
-    </div>
-);
+            </section>
+
+            <section className="mb-4">
+                <h1 className="mb-2">Project Card</h1>
+                <div className="pl-4 flex gap-10 flex-wrap">
+                    <ProjectCard title="Project Title" thumbnailSrc="./placeholder375x375.png">
+                        <p>
+                            Irure occaecat mollit sunt consectetur aliqua minim culpa officia voluptate ipsum in adipisicing consequat est. Ex do enim laboris excepteur sunt officia cupidatat cillum. Proident et eu aliqua consectetur duis qui reprehenderit incididunt.
+                        </p>
+                        <Button className="mt-2">Open GitHub</Button>
+                    </ProjectCard>
+                    <ProjectCard title="Project Title" thumbnailSrc="./placeholder375x375.png">
+                        <p>
+                            Proident eiusmod dolore veniam sint duis sit et quis ut enim duis elit amet consequat. Labore anim ea sit ut. Ex est irure culpa fugiat id. Fugiat eiusmod commodo occaecat duis irure eu dolore et ut tempor. Esse qui commodo minim eiusmod ea magna labore Lorem commodo ullamco. Elit pariatur commodo occaecat commodo magna aliquip. Laborum magna eiusmod et sunt amet laboris in.
+                        </p>
+                        <Button className="mt-2">Open GitHub</Button>
+                    </ProjectCard>
+                </div>
+            </section>
+
+            <section className="mb-4">
+                <h1 className="mb-2">Typography</h1>
+                <div className="pl-4">
+                    <div className="pl-4">
+                        <h1 className="text-5xl font-bold">Title</h1>
+                        <h1>Header 1</h1>
+                        <h2>Header 2</h2>
+                        <p>
+                            This is what a paragraph looks like. Dolor magna exercitation ad ipsum magna anim ut Lorem adipisicing mollit enim anim. Ut ut esse est consectetur pariatur exercitation do occaecat. Cillum eu id mollit est magna proident ullamco amet. Ea reprehenderit laboris dolor non aute officia dolor. Sunt proident officia et dolore mollit est.
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
 
 export default StylesShowcase;
