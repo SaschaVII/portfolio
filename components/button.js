81814546e5f63a43f7d982172a38578ac50ac66c
Sascha Kuhness <skuhness@calendarclub.co.uk>
2023-03-06 10:26:54 +0000

    Add onChange support to Button Component

--- components/button.js
+++ components/button.js
@@ -1,10 +1,10 @@
-const Button = ({children, className, type}) => {
+const Button = ({children, className, onClick, type}) => {
     let styles = className;
 
     if(type === "primary" || type === "" || type === undefined) styles += " btn btn-primary mr-2";
     if(type === "secondary") styles += " btn btn-secondary mr-2";
 
-    return <button className={styles}>{children}</button>;
+    return <button className={styles} onClick={onClick}>{children}</button>;
 };
 
 export default Button;
