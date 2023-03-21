d9bfa1b1688487c6874799f612e2818178bd6a3e
Sascha Kuhness <skuhness@calendarclub.co.uk>
2023-03-21 15:22:24 +0000

    Add about information and link to CV

--- pages/about.js
+++ pages/about.js
@@ -15,15 +15,13 @@ const About = () => {
                     </section>
                     <section className="col-span-12 lg:col-span-8">
                         <h1 className="mb-2">About</h1>
-                        <p className="mb-4">Cillum qui qui reprehenderit cupidatat veniam ad aute aliquip Lorem velit anim et. Dolor proident laboris pariatur nulla proident. Amet esse exercitation reprehenderit labore ullamco elit est veniam et enim et deserunt. Nisi fugiat id sint commodo laborum ipsum sunt ipsum excepteur aliqua.</p>
-                        <p className="mb-4">Officia incididunt consequat nulla ea do aliquip ullamco veniam. Ex laborum adipisicing velit esse tempor minim cupidatat fugiat ut nostrud duis cupidatat laborum. Enim duis Lorem nisi aute enim occaecat. Anim proident eiusmod dolor duis sint velit occaecat. Nostrud Lorem nulla exercitation amet eu et consectetur proident adipisicing laborum Lorem est velit. Veniam mollit exercitation duis non adipisicing culpa anim labore cupidatat sit.</p>
-                        <Button type="primary">Click Me Please</Button>
-                    </section>
-                    <section className="col-span-12 lg:col-span-8">
-                        <h1 className="mb-2">About</h1>
-                        <p className="mb-4">Cillum qui qui reprehenderit cupidatat veniam ad aute aliquip Lorem velit anim et. Dolor proident laboris pariatur nulla proident. Amet esse exercitation reprehenderit labore ullamco elit est veniam et enim et deserunt. Nisi fugiat id sint commodo laborum ipsum sunt ipsum excepteur aliqua.</p>
-                        <p className="mb-4">Officia incididunt consequat nulla ea do aliquip ullamco veniam. Ex laborum adipisicing velit esse tempor minim cupidatat fugiat ut nostrud duis cupidatat laborum. Enim duis Lorem nisi aute enim occaecat. Anim proident eiusmod dolor duis sint velit occaecat. Nostrud Lorem nulla exercitation amet eu et consectetur proident adipisicing laborum Lorem est velit. Veniam mollit exercitation duis non adipisicing culpa anim labore cupidatat sit.</p>
-                        <Button type="primary">Click Me Please</Button>
+                        <p className="mb-4">
+                            I am a hardworking individual currently seeking a new entry level position as a Software Tester/Developer. Having worked on the code for multiple projects, I have made myself very well versed in object oriented programming languages.
+                        </p>
+                        <p>
+                            Recent achievements include the coding of an Arduino chip, serving as a MIDI Interface, for my diploma thesis at HTL Bulme.
+                        </p>
+                        <a className="btn btn-secondary mr-2 inline-block mt-4" href="/cv_7_01_2023.pdf" target="_blank">Check out my CV</a>
                     </section>
                 </div>
             </div>
