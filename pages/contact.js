46e0a15d10fd3d8a9147c302cb6c867706cde406
Sascha Kuhness <skuhness@calendarclub.co.uk>
2023-03-21 15:39:53 +0000

    Add contact info and links

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 const Contact = () => {
     return (
@@ -7,13 +8,30 @@ const Contact = () => {
                 <title>Sascha Kuhness - Contact</title>
             </Head>
             <div className="container mt-5 sm:mt-20">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div className="grid grid-cols-1 gap-4">
                     <div className="grid grid-cols-1 md:grid-cols-1 gap-4">
                         <section>
-                            <h1>Contact</h1>
-                            <p>
-                                Nostrud ex do Lorem mollit deserunt in excepteur cillum fugiat commodo minim voluptate velit aliqua. Eu Lorem dolor magna dolore ullamco laboris dolore duis. Eu sunt et duis proident velit non adipisicing commodo nisi nisi anim pariatur labore exercitation. Minim adipisicing irure culpa est occaecat.
+                            <h1>Let&apos;s Talk!</h1>
+                            <p className="mt-2">
+                                I am always happy to talk about any job opportunities or other quries via any of the following methods:
                             </p>
+                            <ul className="list-disc mt-4 list-inside">
+                                <li>
+                                    <a href="//www.github.com/SaschaVII" target="_blank" className="inline-block mb-2 hover:text-slate-500 dark:text-slate-400 dark:hover:text-slate-200">
+                                        <div><FaGithub /> <span>Github</span></div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="//www.linkedin.com/in/sascha-kuhness-34968a132" target="_blank" className="inline-block mb-2 hover:text-slate-500 dark:text-slate-400 dark:hover:text-slate-200">
+                                        <div><FaLinkedin /> <span>LinkedIn</span></div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="mailto:sascha.kuhness@gmail.com?subject=Mail from Portfolio" className="inline-block mb-2 hover:text-slate-500 dark:text-slate-400 dark:hover:text-slate-200">
+                                        <div><FaEnvelope /> <span>Email</span></div>
+                                    </a>
+                                </li>
+                            </ul>
                         </section>
                     </div>
                 </div>
