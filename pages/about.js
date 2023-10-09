9fb9299b4146f2ffb867e80fc8bc655dc2608cd7
Sascha <sascha.kuhness@gmail.com>
2023-10-09 15:29:34 +0100

    Update CV link in about.js

--- pages/about.js
+++ pages/about.js
@@ -24,7 +24,7 @@ const About = () => {
                         <p className="mb-4">
                             I am excited to bring my skills and expertise to a new opportunity where I can contribute to the development and success of innovative projects. Thank you for considering my application.
                         </p>
-                        <a className="btn btn-secondary mr-2 inline-block" href="/cv_7_01_2023.pdf" target="_blank">Check out my CV</a>
+                        <a className="btn btn-secondary mr-2 inline-block" href="/Sascha_Kuhness_CV_2023-10-09.pdf" target="_blank">Check out my CV</a>
                     </section>
                 </div>
             </div>
