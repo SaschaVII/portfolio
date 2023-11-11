441b839251e299a8d1bb94cf3c49b2f8a766b379
SaschaVII <sascha.kuhness@gmail.com>
2023-11-11 00:48:52 +0000

    Add ipa.json

--- pages/ipa.js
+++ pages/ipa.js
@@ -1,12 +1,29 @@
 import IpaCard from "@/components/ipaCard";
 import IpaMerger from "@/components/ipaMerger";
 import Head from "next/head";
-import { useState } from "react";
+import { parse } from "postcss";
+import { useState, useEffect } from "react";
 
 const Contact = () => {
+    const [ipaData, setIpaData] = useState(null);
     const [mergedString, setMergedString] = useState("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/ipa.json");
+                const data = await response.json();
+                setIpaData(data);
+            } catch (error) {
+                console.error('Error fetching IPA data:', error);
+            }
+        };
+        fetchData();
+    }, []);
+
     const addToIpaMerger = symbol => setMergedString(current => current + symbol);
     const deleteMergedString = () => setMergedString("");
+
     return (
         <>
             <Head>
@@ -17,37 +34,21 @@ const Contact = () => {
                     <h1 className="mb-2 inline-block is-underlined">IPA Symbols</h1>
                     <h2>Consonants</h2>
                     <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-8 gap-4 py-2 mb-4">
-                        <IpaCard title="&#x03B8;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x00F0;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x0283;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x0292;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x014B;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x0294;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x0279;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x02A4;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x02A7;" clickHandler={addToIpaMerger} />
+                    {ipaData?.consonants?.map((text, index) => (
+                        <IpaCard key={index} title={text} clickHandler={addToIpaMerger} />
+                    ))}
                     </div>
                     <h2>Vowels</h2>
                     <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-8 gap-4 py-2 mb-4">
-                        <IpaCard title="&#x026A;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x028A;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x025B;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x025C;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x028C;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x0254;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x0251;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x0252;" clickHandler={addToIpaMerger} />
+                    {ipaData?.vowels?.standard?.map((text, index) => (
+                        <IpaCard key={index} title={text} clickHandler={addToIpaMerger} />
+                    ))}
                     </div>
                     <h3>Diphthongs</h3>
                     <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-8 gap-4 py-2">
-                        <IpaCard title="e&#x026A;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="o&#x028A;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="a&#x028A;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x026A;&#x0259;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="e&#x0259;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x0254;&#x026A;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="a&#x026A;" clickHandler={addToIpaMerger} />
-                        <IpaCard title="&#x028A;&#x0259;" clickHandler={addToIpaMerger} />
+                    {ipaData?.vowels?.diphthongs?.map((text, index) => (
+                        <IpaCard key={index} title={text} clickHandler={addToIpaMerger} />
+                    ))}
                     </div>
                     {(mergedString) && 
                         <>
