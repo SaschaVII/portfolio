4108d04ce33c9c3dca1cc35548cf0fa8c65b9dc1
SaschaVII <sascha.kuhness@gmail.com>
2023-12-01 14:34:20 +0000

    Add hiover animation to project cards

--- components/projectCard.js
+++ components/projectCard.js
@@ -1,23 +1,38 @@
-const ProjectCard = ({ children, className, title, thumbnailSrc, projectUrl }) => {
+import { motion } from "framer-motion";
+
+const ProjectCard = ({ children, className, title, thumbnailSrc, projectUrl, animation }) => {
     // TODO: make sure that the card has a max height and scales the thumbnail to a default size
     // TODO: add pretty child component for tech used
 
     let styles = className;
     styles += " group max-w-[375px] bg-white hover:shadow-lg transition-shadows ease-in-out duration-100 dark:bg-transparent dark:hover:ring-2 ring-slate-700";
 
-    return (
-        <div className={styles}>
-            <a href={projectUrl || ""} target="_blank">
-                <img className="object-cover w-[375px] h-[375px] saturate-0 brightness-150 group-hover:saturate-100 group-hover:brightness-100 transition ease-in-out duration-500" src={thumbnailSrc} alt={title + ' thumbnail'} />
-            </a>
-            <div className="p-5 flex flex-col">
-                <h1 className="mb-2">{title}</h1>
-                <div>
-                    {children}
+    const generateProjectCard = () => {
+        return (
+            <div className={styles}>
+                <a href={projectUrl || ""} target="_blank">
+                    <img className="object-cover w-[375px] h-[375px] saturate-50 brightness-100 group-hover:saturate-100 group-hover:brightness-150 transition ease-in-out duration-500" src={thumbnailSrc} alt={title + ' thumbnail'} />
+                </a>
+                <div className="p-5 flex flex-col">
+                    <h1 className="mb-2">{title}</h1>
+                    <div>
+                        {children}
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+
+    if (animation) {
+        return (
+            <motion.div 
+                whileHover={{ scale: 1.05 }}>
+                {generateProjectCard()}
+            </motion.div>
+        );
+    } else {
+        return generateProjectCard();
+    }          
 };
 
 export default ProjectCard;
--- pages/work.js
+++ pages/work.js
@@ -29,27 +29,27 @@ const Work = () => {
                 <section>
                     <h1 className="mb-2 inline-block is-underlined">Work</h1>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                        <ProjectCard title="This Portfolio" projectUrl="/about" thumbnailSrc="/portfolio_proj_thumbnail.png">
+                        <ProjectCard title="This Portfolio" projectUrl="/about" thumbnailSrc="/portfolio_proj_thumbnail.png" animation={true}>
                             <p>
                                 This portfolio website is a showcase of my professional experience, skills, and projects. Built entirely from scratch using <strong>React</strong> and <strong>Next.js</strong>, it features a sleek and modern design that I crafted myself using <strong>Tailwind</strong>. Visitors can browse my past work and explore my expertise in various areas.
                             </p>
                             <Button className="mt-2" type="secondary" onClick={handlePortfolioClick}><FaGithub /> GitHub</Button>
                         </ProjectCard>
-                        <ProjectCard title="Sticky Notes" projectUrl="https://saschavii.github.io/StickyNotesApp/" thumbnailSrc="/stickyNotes_proj_thumbnail.png">
+                        <ProjectCard title="Sticky Notes" projectUrl="https://saschavii.github.io/StickyNotesApp/" thumbnailSrc="/stickyNotes_proj_thumbnail.png" animation={true}>
                             <p>
                                 Introducing my minimalist sticky notes website, created using vanilla <strong>JavaScript</strong>, <strong>CSS</strong>, and <strong>HTML</strong>. With a clean and efficient interface, the app enables you to create notes effortlessly by clicking anywhere on the canvas, saving notes inbetween sessions via the local storage api.
                             </p>
                             <Button className="mt-2" type="secondary" onClick={handleStickyNotesClick}><FaGithub /> GitHub</Button>
                         </ProjectCard>
-                        <ProjectCard title="Simple Task List" projectUrl="https://saschavii.github.io/task-list-app" thumbnailSrc="/taskListApp_thumbnail.png">
+                        <ProjectCard title="Simple Task List" projectUrl="https://saschavii.github.io/task-list-app" thumbnailSrc="/taskListApp_thumbnail.png" animation={true}>
                             <p>A simple Next.js powered task list that is meant to increase productivity. It smartly orders tasks based on pin-status, priority and due date, highlighting the the most important task for you. Storage is implemented using the browser&#39;s local storage api.</p>
                             <Button className="mt-2" type="secondary" onClick={handleTaskListAppProjClick}><FaGithub /> GitHub</Button>
                         </ProjectCard>
-                        <ProjectCard title="IPA Keyboard" projectUrl="/ipa" thumbnailSrc="/ipa_proj_thumbnail.png    ">
+                        <ProjectCard title="IPA Keyboard" projectUrl="/ipa" thumbnailSrc="/ipa_proj_thumbnail.png" animation={true}>
                             <p>An keyboard that automatically copies the International Phonetic Alphabet symbols to your clipboard, created with <strong>React</strong>.</p>
                             <Button className="mt-2" type="secondary" onClick={handleIpaProjClick}><FaGithub /> GitHub</Button>
                         </ProjectCard>
-                        <ProjectCard title="More to come soon" thumbnailSrc="https://placekitten.com/375/375">
+                        <ProjectCard title="More to come soon" thumbnailSrc="https://placekitten.com/375/375" animation={true}>
                             <p>I am currently in the process of adding all my the projects I have been working on...</p>
                         </ProjectCard>
                         {/* <ProjectCard title="Project Title 3" thumbnailSrc="https://placekitten.com/375/375">
